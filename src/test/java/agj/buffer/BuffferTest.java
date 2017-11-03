3ef060b5d7b51ea0f6a0b71287d54daa39af2d44
andres <andresgj@uma.es>
2017-11-03 19:52:11 +0100

    Final depurado

--- src/test/java/agj/buffer/BuffferTest.java
+++ src/test/java/agj/buffer/BuffferTest.java
@@ -11,95 +11,41 @@ public class BuffferTest {
   @Test
   public void crearBuffer() {
     int capacity = 4;
-   Buffer <Double> bufferTest = new Buffer<>(capacity);
+    Buffer<Double> bufferTest = new Buffer<>(capacity);
     assertNotNull(bufferTest);
-    assertEquals(capacity,  bufferTest.getCapacity());
+    assertEquals(capacity, bufferTest.getCapacity());
     assertEquals(capacity, bufferTest.getNumberOfHoles());
-    
-  }
-  @ Test (expected =BufferException.class)
-  public void ShouldGetOneBufferException() {
-    int capacity = 2;
-   Buffer <String> bufferTest = new Buffer<>(capacity);
-   bufferTest.get();
+
   }
 
-  @ Test (expected =BufferException.class)
+  @Test(expected = BufferException.class)
+  public void ShouldGetOneBufferException() {
+    int capacity = 2;
+    Buffer<String> bufferTest = new Buffer<>(capacity);
+    bufferTest.get();
+  }
+
+  @Test(expected = BufferException.class)
   public void ShouldPutOnAFullBufferRaiseException() {
     int capacity = 2;
-   Buffer <Double> bufferTest = new Buffer<>(capacity);
-   bufferTest.put(2.3);
-   bufferTest.put(3.1);
-   bufferTest.put(4.2);
+    Buffer<Double> bufferTest = new Buffer<>(capacity);
+    bufferTest.put(2.3);
+    bufferTest.put(3.1);
+    bufferTest.put(4.2);
   }
+
   @Test
   public void putOneElementNoEmptyBuffer() {
     int capacity = 4;
-   Buffer <Double> bufferTest = new Buffer<>(capacity);
-   bufferTest.put(2.3);
-   int numberOfelemennts = bufferTest.getNumberOfElements();
-   
-   assertEquals(1, numberOfelemennts);
+    Buffer<Double> bufferTest = new Buffer<>(capacity);
+    bufferTest.put(2.3);
+    int numberOfelemennts = bufferTest.getNumberOfElements();
+
+    assertEquals(1, numberOfelemennts);
   }
-         
+
   @Test
   public void crearBufferZero() {
     bufferTest = new Buffer(0);
   }
-
-  @Test
-  public void putNewElement() {
-    crearBuffer();
-    bufferTest.put(2.1);
-  }
-
-  @Test
-  public void countgetNumberOfOperations() {
-    crearBuffer();
-    bufferTest.put(element);
-    System.out.println(bufferTest.getNumberOfOperations());
-  }
-
-  @Test
-  public void getElementFromNoZero() throws BufferException {
-    crearBuffer();
-    bufferTest.put(element);
-    bufferTest.put(element);
-    bufferTest.get();
-  }
-
-  @Test
-  public void getElementFromZero() throws BufferException {
-    crearBuffer();
-    bufferTest.put(element);
-    bufferTest.put(element);
-    bufferTest.get();
-  }
-
-  @Test
-  public void tamBuffer() {
-    crearBuffer();
-    bufferTest.put(element);
-    bufferTest.put(element);
-    System.out.println(bufferTest.getNumberOfElements());
-
-  }
-
-  @Test
-  public void tamHoles() {
-    crearBuffer();
-    bufferTest.put(element);
-    System.out.println(bufferTest.getNumberOfHoles());
-  }
-
-  @Test
-  public void tamCapacity() {
-    crearBuffer();
-    bufferTest.put(element);
-    System.out.println(bufferTest.getCapacity());
-  }
-
-
-
 }
-
